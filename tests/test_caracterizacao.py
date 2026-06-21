287bb4d26c7f585f4fa2f32b51ea571eab477ce0
Gabriel Holanda <gholanda.alb@gmail.com>
2026-06-21 14:42:36 -0300

    test: adiciona o teste de caracterização para itens normais
    Foram realizados 3 casos testes:
    -condições padrões
    -validade menor que 0, qualidade diminui 2x
    -qualidade nunca ser menor que 0

---
+++ tests/test_caracterizacao.py
@@ -0,0 +1,29 @@
+import pytest
+from gilded_rose_completo import GildedRose, Item
+
+def test_itens_normais():
+    #arrange
+
+    itens = [
+        Item("Espada normal", validade = 10, qualidade= 20),
+        Item("Espada normal", validade = -1, qualidade= 30),
+        Item("Espada normal", validade = 5, qualidade = 0)
+    ]
+    rose = GildedRose(itens)
+
+    #act
+
+    rose.atualizar_qualidade()
+
+    #assert
+
+    assert itens[0].validade == 9
+    assert itens[0].qualidade == 19
+
+    #teste garante que após a validade expirar a qualidade diminui 2x
+    assert itens[1].validade == -2
+    assert itens[1].qualidade == 28
+
+    #teste garante que que a qualidade não fique negativa
+    assert itens[2].validade == 4
+    assert itens[2].qualidade == 0
